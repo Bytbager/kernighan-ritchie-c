672b0a5985a9777287519f52285279c2c9ed2a57
Державин Егор Иванович <egor.derzhavin@eltex.loc>
2025-06-18 14:51:52 +0700

    Added exercises for chapter 1 subchapter 5 section 1

---
+++ chapter-1/subchapter-5/section-1/exercise-2.c
@@ -0,0 +1,11 @@
+/*
+Нужно написать программу, печатающую значение EOF
+*/
+#include <stdio.h>
+int main()
+{
+    printf("Значение EOF: %d\n", EOF);
+    putchar(EOF);
+    putchar('\n');
+    return 0;
+}
